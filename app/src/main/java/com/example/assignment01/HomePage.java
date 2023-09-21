3afd800803adfac92c40bfaad8c032097dfe0eb5
wasiahmad03092003 <wasiahmadsiddiqui0@gmail.com>
2023-09-21 20:17:13 +0500

    update

--- app/src/main/java/com/example/assignment01/HomePage.java
+++ app/src/main/java/com/example/assignment01/HomePage.java
@@ -12,6 +12,6 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        Toast.makeText(HomePage.this, "Account ban gaya ha", Toast.LENGTH_SHORT).show();
+        Toast.makeText(HomePage.this, "Account has been created", Toast.LENGTH_SHORT).show();
     }
 }
